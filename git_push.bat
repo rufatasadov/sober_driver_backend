2c4efd5be50978818b134d3cd680871c0e61d6bc
= <=>
2025-08-06 14:54:12 +0400

    'auto'

--- git_push.bat
+++ git_push.bat
@@ -1,25 +1,6 @@
-@echo off
-echo ========================================
-echo           GIT PUSH SCRIPT
-echo ========================================
-echo.
-
-
-:: Commit mesajı al
-set /p commit_msg="Commit message: "
-echo.
-
-:: Commit et
-echo [3/5] Commit edilir...
-git commit -m "%commit_msg%"
-echo.
-
-
-:: Push et
-echo [5/5] Push edilir...
-git push
-echo.
-
+git add .
+git commit -m 'auto'
+git push origin main
 echo ========================================
 echo           TAMAMLANDI!
  
